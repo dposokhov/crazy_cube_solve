524ebacda216481142f2564abd637f6594c12c3c
dposokhov <30771908+dposokhov@users.noreply.github.com>
2017-10-30 01:08:25 +0300

    Update bot.py
    New buttons
    added /solve, /color-solve
    added /text-solve, /text-color-solve
    added support lowercase letters
    added new /help

--- bot.py
+++ bot.py
@@ -3,20 +3,41 @@ import telebot
 import kociemba
 
 token = 'TOKEN'
-
 bot = telebot.TeleBot(token)
 
-markup = telebot.types.ReplyKeyboardMarkup()
-markup.row_width
+markup = telebot.types.ReplyKeyboardMarkup(True, False)
+
 markup.row('/help', '/example')
+markup.row('/#solve', '/#color-solve')
+markup.row('/#text-solve', '/#text-color-solve')
 
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-    start = 'https://github.com/dposokhov/kubik_solve_tlgrm \n' \
-            'На вход бот получает строку из 54 символов. Если бы кубик рубика был собран, то это строка \n' \
-            'выглядела бы так UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB'
+    start = """
+https://github.com/dposokhov/crazy_cube_solve
+
+
+/solve:
+На вход бот получает строку из 54 символов.
+Если бы кубик рубика был собран, то строка выглядела бы так
+UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB
+
+
+/color_solve
+На вход бот получает строку из 54 символов.                
+Кубик расположить White == Up, Blue == Front, Red == Left             
+White, Orange, Blue, Yellow, Red, Green.                   
+WWWWWWWWWOOOOOOOOOBBBBBBBBBYYYYYYYYYRRRRRRRRRGGGGGGGGG     
+THE=====END      
+                                                                """
     bot.send_message(message.chat.id, start, reply_markup=markup)
+
+
+@bot.message_handler(commands=['#solve', '#color-solve', '#text-solve', '#text-color-solve'])
+def print_text(message):
+    bot.send_message(message.chat.id, 'Эта кнопка предназначена для ознакомления с функцией. После неё нужно '
+                                      'написать конфигурацию кубика(54 символа)')
 
 
 @bot.message_handler(commands=['example'])
@@ -35,15 +56,84 @@ def print_example(message):
 
 @bot.message_handler(commands=['solve'])
 def solve(message):
-    data = message.text.split()[-1]
+    data = message.text.split()[-1].upper()
     if 'U' and 'R' and 'F' and 'D' and 'L' and 'B' in list(data) and len(data) == 54:
-        out = kociemba.solve(message.text.split()[-1])
+        out = kociemba.solve(data)
         bot.send_message(message.chat.id, out, reply_markup=markup)
         for item in out.split():
             f = open('./kubik/{}.jpg'.format(item), 'rb')
             bot.send_photo(message.chat.id, f)
     else:
-        bot.send_message(message.chat.id, 'Введены неверные входные данные', reply_markup=markup)
+        if len(data) > 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит > 54 символов',
+                             reply_markup=markup)
+        elif len(data) < 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит < 54 символов',
+                             reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Должно быть {U, R, F, D, L, B}',
+                             reply_markup=markup)
+
+
+@bot.message_handler(commands=['text-solve'])
+def solve(message):
+    data = message.text.split()[-1].upper()
+    if 'U' and 'R' and 'F' and 'D' and 'L' and 'B' in list(data) and len(data) == 54:
+        out = kociemba.solve(data)
+        bot.send_message(message.chat.id, out, reply_markup=markup)
+    else:
+        if len(data) > 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит > 54 символов',
+                             reply_markup=markup)
+        elif len(data) < 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит < 54 символов',
+                             reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Должно быть {U, R, F, D, L, B}',
+                             reply_markup=markup)
+
+
+@bot.message_handler(commands=['color-solve'])
+def solve(message):
+    data = message.text.split()[-1].upper()
+    for old, new in (('W', 'U'), ('B', 'F'), ('R', 'L'), ('O', 'R'), ('G', 'B'), ('Y', 'D')):
+        data = data.replace(old, new)
+    if 'U' and 'R' and 'F' and 'D' and 'L' and 'B' in list(data) and len(data) == 54:
+        out = kociemba.solve(data)
+        bot.send_message(message.chat.id, out, reply_markup=markup)
+        for item in out.split():
+            f = open('./kubik/{}.jpg'.format(item), 'rb')
+            bot.send_photo(message.chat.id, f)
+    else:
+        if len(data) > 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит > 54 символов',
+                             reply_markup=markup)
+        elif len(data) < 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит < 54 символов',
+                             reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Должно быть {W, B, R, O, G, Y}',
+                             reply_markup=markup)
+
+
+@bot.message_handler(commands=['text-color-solve'])
+def solve(message):
+    data = message.text.split()[-1].upper()
+    for old, new in (('W', 'U'), ('B', 'F'), ('R', 'L'), ('O', 'R'), ('G', 'B'), ('Y', 'D')):
+        data = data.replace(old, new)
+    if 'U' and 'R' and 'F' and 'D' and 'L' and 'B' in list(data) and len(data) == 54:
+        out = kociemba.solve(data)
+        bot.send_message(message.chat.id, out, reply_markup=markup)
+    else:
+        if len(data) > 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит > 54 символов',
+                             reply_markup=markup)
+        elif len(data) < 54:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Строка содержит < 54 символов',
+                             reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Введены неверные входные данные: Должно быть {W, B, R, O, G, Y}',
+                             reply_markup=markup)
 
 
 if __name__ == '__main__':
